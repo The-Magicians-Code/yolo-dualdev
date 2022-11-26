7d164d59e27c153195d4b6cc76f12c653e4ccc68
Tanel Treuberg <60236942+The-Magicians-Code@users.noreply.github.com>
2022-11-26 21:59:44 +0200

    Update shellexport.sh

--- userexports/shellexport.sh
+++ userexports/shellexport.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Define YOLOv5 model type
-declare -a models=("n" "s" "m" "l" "x" "n6" "s6" "m6" "l6" "x6")
+declare -a models=("n" "s") #"m" "l" "x" "n6" "s6" "m6" "l6" "x6")
 # Model input size (a x a)
-size=1280
+size=640
 # Input batch size
-batch=3
+batch=1
 
 echo "Making the folder for models"
 mkdir models_"$size"x"$size"_batch_"$batch"
@@ -12,6 +12,8 @@ echo "Starting model exports"
 for i in "${models[@]}"
 do
    python ../export.py --weights yolov5"$i".pt --batch-size "$batch" --include onnx --imgsz "$size"
+   mv yolov5"$i".onnx yolov5"$i"_"$size"x"$size"_batch_"$batch".onnx
+   mv yolov5"$i".pt yolov5"$i"_"$size"x"$size"_batch_"$batch".pt
 done
 echo "Moving exported models to user specified folder"
 mv yolov5*.* models_"$size"x"$size"_batch_"$batch"/
