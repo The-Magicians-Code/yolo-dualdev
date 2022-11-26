8297682a56c38dea5a13af62b8fda14e1cf87403
Tanel Treuberg <tanetre@gmail.com>
2022-11-26 18:00:32 +0200

    Fixed export_cont.sh bug

--- export_cont.sh
+++ export_cont.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build -t YOLO_models .
-docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -it --rm -v $PWD/userexports:/usr/src/app/hostitems -p 3001:3001 YOLO_models
+docker build -t yolo_models .
+docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -it --rm -v $PWD/userexports:/usr/src/app/hostitems -p 3001:3001 yolo_models
