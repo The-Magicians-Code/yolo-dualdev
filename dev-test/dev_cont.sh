c51d828d02f13341e39f73300d92d5cde2366cd1
Tanel Treuberg <tanetre@gmail.com>
2022-11-26 19:19:00 +0200

    Run containers in detached mode

--- dev-test/dev_cont.sh
+++ dev-test/dev_cont.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker build -t pc_dev .
-docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -it --rm -v $PWD/production:/workspace/torching -p 3000:3000 pc_dev
+docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -d --rm -v $PWD/production:/workspace/torching -p 3000:3000 pc_dev
 # docker run -it --rm -p 3000:3000 PC_dev
