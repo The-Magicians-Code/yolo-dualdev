398fff00caf91f0a2ecd9ab7df0d152d423ebf74
Tanel Treuberg <60236942+The-Magicians-Code@users.noreply.github.com>
2022-11-27 07:47:18 +0200

    Fixed video decoder pipeline for x86 systems

--- dev-test/production/inference.py
+++ dev-test/production/inference.py
@@ -8,78 +8,97 @@ app = Flask(__name__)
 
 model_path = "models/yolov5m6_640x640_batch_1.engine"
 
-# model = torch.hub.load('ultralytics/yolov5', 'yolov5s6', pretrained=True)
 model = torch.hub.load("ultralytics/yolov5", "custom", model_path) # This line is important since it contains RT execution
 in_size = model.model.bindings["images"].shape[-1]  # Retrieve input size of the model
 model.eval().to("cuda")
 
-# cap = cv2.VideoCapture('filesrc location=../video.mp4 ! qtdemux ! queue ! h264parse ! nvv4l2decoder ! nvvidconv ! video/x-raw,format=BGRx,width=1280,height=720 ! queue ! videoconvert ! queue ! video/x-raw, format=BGR ! appsink', cv2.CAP_GSTREAMER)
-cap = cv2.VideoCapture("video.mp4")
+# For x86 systems
+cam0 = cv2.VideoCapture('filesrc location=video.mp4 ! qtdemux ! queue ! h264parse ! avdec_h264 ! videoconvert ! video/x-raw,format=BGRx,width=1280,height=720 ! queue ! videoconvert ! queue ! video/x-raw, format=BGR ! appsink', cv2.CAP_GSTREAMER)
+# For Jetson
+# cam0 = cv2.VideoCapture('filesrc location=video.mp4 ! qtdemux ! queue ! h264parse ! nvv4l2dec ! nvvidconv ! video/x-raw,format=BGRx,width=1280,height=720 ! queue ! videoconvert ! queue ! video/x-raw, format=BGR ! appsink', cv2.CAP_GSTREAMER)
+# cam0 = cv2.VideoCapture("video.mp4")
+
+# colour = (B, G, R)
+colour = (0, 140, 255)
+thickness = 4
+fontscale = 0.75
+fontthick = 2
+
+def openstreams(cameras):
+    """Open every available stream and return it's state and current frame
+
+    Args:
+        cameras (list): list of cv2 VideoCapture objects
+
+    Returns:
+        states, frames (list, list): stream status True / False and current frame
+    """
+    states, frames = zip(*[camera.read() for camera in cameras])
+    return states, frames
+
+def plotdetections(detection, stream):
+    height, width = stream.shape[:2]
+    for i in range(detection.shape[0]):    
+        if detection.iloc[i]["class"] != 8: # 8 - boat, ship, vessel
+            continue
+
+        xmin = int(detection.iloc[i]["xmin"] / (in_size) * (width))
+        xmax = int(detection.iloc[i]["xmax"] / (in_size) * (width))
+        ymin = int(detection.iloc[i]["ymin"] / (in_size) * (height))
+        ymax = int(detection.iloc[i]["ymax"] / (in_size) * (height))
+        # print(xmin, xmax)
+        confidence = detection.iloc[i]['confidence']
+        score_txt = f"{int(confidence * 100.0)}%"
+        
+        # label = "" # detection.iloc[i]["name"]
+        
+        cv2.rectangle(stream, (xmin, ymin), (xmax, ymax), (0, int(confidence * 255), int(255 - confidence * 255)), thickness)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        # (w, h), _ = cv2.getTextSize(
+        #         f"{label}: " + score_txt, font, fontscale, fontthick)
+
+        # This block for seeing the values on detection boxes
+        # cv2.rectangle(streams, (xmin, ymax - h - 10), (xmin + w, ymax), colour, -1) # -1 to fill the rectangle
+        # cv2.putText(stream, f"{label}: {score_txt}", (xmin, ymax - 5), font, fontscale, (255, 255, 255), fontthick, cv2.FILLED)
+        cv2.putText(stream, score_txt, (xmin, ymax - 5), font, fontscale, (255, 255, 255), fontthick, cv2.FILLED)
+    
+    return stream
 
 def gen_frames():
-    ret, frame = cap.read()
-    height, width = frame.shape[:2]
-
+    online, frame = openstreams([cam0])
+    height, width = frame[0].shape[:2]
+    streams_ok = all(online)
     fps = 0
     tau = time.time()
     smoothing = 0.9
-    text_pos = (32, height - 32)
-
-    # Parameters for drawing
-    #colour = (B, G, R)
-    colour = (0, 140, 255)
-    thickness = 4
-    fontscale = 0.5
-    fontthick = 1
+    fps_pos = (32, height - 32)
+    top_left = (32, 38)
 
     with torch.no_grad():
-        while(ret):
+        while streams_ok:
             #Capture frame-by-frame
-            ret, pic = cap.read()
-            
+            online, streams = openstreams([cam0])
+            streams_ok = all(online)
             # FPS calculation
             now = time.time()
             if now > tau:  # avoid div0
                 fps = fps*smoothing + 0.1/(now - tau)
             tau = now
             
-            pic = cv2.resize(pic, (in_size, in_size))
-            results = model(pic, size=in_size)
-            detections = results.pandas().xyxy[0]
+            inputs = [cv2.resize(stream, (in_size, in_size)) for stream in streams]
+            results = model(inputs, size=in_size)
+            detections = results.pandas().xyxy
 
-            for i in range(detections.shape[0]):
-                if detections.iloc[i]["class"] != 8:
-                    continue
-
-                xmin = int(detections.iloc[i]["xmin"])
-                xmax = int(detections.iloc[i]["xmax"])
-                ymin = int(detections.iloc[i]["ymin"])
-                ymax = int(detections.iloc[i]["ymax"])
-
-                score_txt = f"{round(detections.iloc[i]['confidence']*100.0, 2)}%"
-                
-                label = detections.iloc[i]["name"]
-
-                cv2.rectangle(pic, (xmin, ymin), (xmax, ymax), colour, thickness)
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                (w, h), _ = cv2.getTextSize(
-                        f"{label}: " + score_txt, font, fontscale, fontthick)
-
-                # This block for seeing the values on detection boxes
-                # cv2.rectangle(pic, (xmin, ymax - h - 10), (xmin + w, ymax), colour, -1) # -1 to fill the rectangle
-                cv2.putText(pic, f"{label}: " + score_txt, (xmin, ymax - 5), font, fontscale, (255, 255, 255), fontthick, cv2.FILLED)
-
+            streams = [plotdetections(detection, stream) for detection, stream in zip(detections, streams)]
             # Display fps
-            cv2.putText(pic, str(int(fps)), \
-                        text_pos, \
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 2)
-            
-            ret, buffer = cv2.imencode('.jpg', pic)
-            pic = buffer.tobytes()
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            [cv2.putText(stream, pos, top_left, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 140, 255), 2) for pos, stream in zip(["Port", "Bow", "Starboard"], streams)]
+            outs = cv2.hconcat([cv2.putText(stream, f"{int(fps)}", fps_pos, cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 2) for stream in streams])
+            online, buffer = cv2.imencode('.jpg', outs)
+            outs = buffer.tobytes()
+            # if cv2.waitKey(1) & 0xFF == ord('q'):
+            #     break
             yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + pic + b'\r\n')  # concat frame one by one and show result
+                b'Content-Type: image/jpeg\r\n\r\n' + outs + b'\r\n')  # concat frame one by one and show result
             # print(f"{fps:.2f}")
             
 @app.route('/video_feed')
