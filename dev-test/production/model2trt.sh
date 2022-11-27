dfb01508b969fa4748740fb1cf4caa5f1190120b
Tanel Treuberg <60236942+The-Magicians-Code@users.noreply.github.com>
2022-11-27 06:35:49 +0200

    Optional workspace variable introduced

--- dev-test/production/model2trt.sh
+++ dev-test/production/model2trt.sh
@@ -6,4 +6,4 @@ fi
 # User defined ONNX model path without .onnx suffix
 model="models/yolov5m6_640x640_batch_1"
 workspace_size=$((1<<12))
-trtexec --onnx="$model".onnx --saveEngine="$model".engine --inputIOFormats=fp16:chw --outputIOFormats=fp16:chw --fp16 --workspace=$workspace_size
+trtexec --onnx="$model".onnx --saveEngine="$model".engine --inputIOFormats=fp16:chw --outputIOFormats=fp16:chw --fp16 # --workspace=$workspace_size
