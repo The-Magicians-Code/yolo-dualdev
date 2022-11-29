77e488f3def31c79b2f0fe5c6837368cabd0e1b7
Tanel Treuberg <60236942+The-Magicians-Code@users.noreply.github.com>
2022-11-29 02:08:37 +0200

    Inference update
    Updates, recording output works, saving does not

--- dev-test/production/inference.py
+++ dev-test/production/inference.py
@@ -3,34 +3,41 @@
 # @Github: https://github.com/The-Magicians-Code
 # @Script: inference.py
 # @Description: Perform inference on user defined input streams, using preconfigured YOLOv5 TensorRT model
-# @Last modified: 2022/11/27
+# @Last modified: 2022/11/29
 
 import cv2
 import time
 import torch
 import argparse
 import platform
+from pathlib import Path
 from flask import Flask, render_template, Response
 
 parser = argparse.ArgumentParser(description="Neural Network inference on video stream(s)")
+parser.add_argument('--rec', help="Record the stream into a video file --rec out.mp4")
 parser.add_argument('--input-video', required=True, nargs="+", help="Read video file(s) --input-video video0.mp4 video1.mp4 ...")
-# parser.add_argument('--no-model', action='store_true', help="Do not run a model")
 parser.add_argument('--model', help="YOLOv5 unoptimised Neural Network for object detection --model yolov5m6")
 parser.add_argument('--imsize', help="YOLOv5 unoptimised Neural Network input size --imsize 640")
 parser.add_argument('--perf', action='store_true')
-# parser.add_argument('--no-perf', dest='perf', action='store_false')
-parser.set_defaults(perf=False)
 parser.add_argument('--rt-model', help="YOLOv5 RT Neural Network for object detection --rt-model yolov5m6_640x640_batch_1")
 args = parser.parse_args()
 
 app = Flask(__name__)
 
-# if args.input_video:
-videos = args.input_video
-# else:
-    # videos = ["video.mp4", "videoplay.mp4", "videoplayback.mp4"]
+if args.rec:
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video_file = Path().cwd() / args.rec
+    print("Saving output video to:", video_file)
+
+if args.input_video:
+    videos = args.input_video
+else:
+    parser.error("--input-video requires an argument")
 
 if args.rt_model:
+    if not Path(f"models/{args.rt_model}.engine").exists():
+        raise FileNotFoundError(f"Did not find RT model {args.rt_model}.engine in /models/ folder. Specify model name ONLY without .engine suffix and make sure it is located in models/ folder")
+    
     model = torch.hub.load("ultralytics/yolov5", "custom", f"models/{args.rt_model}.engine") # This line is important since it contains RT execution
     input_params = model.model.bindings["images"].shape  # Retrieve input size of the model
     img_size = input_params[-1] # One dimension since n x n shape
@@ -44,15 +51,18 @@ elif args.model and not args.imsize:
     
 model.eval().to("cuda")
 
-# For x86 systems
-if platform.machine() == "x86_64":
-    cams = [cv2.VideoCapture(f'filesrc location={video} ! qtdemux ! queue ! h264parse ! avdec_h264 ! videoconvert ! video/x-raw,format=BGRx,width=1280,height=720 ! queue ! videoconvert ! queue ! video/x-raw, format=BGR ! appsink', cv2.CAP_GSTREAMER) for video in videos]
+if platform.machine() == "x86_64":  # For PC
+    decoder = "avdec_h264"
+    video_converter = "videoconvert"
     app_port = 3000
-
-# For Jetson
-elif platform.machine() == "aarch64":
-    cams = [cv2.VideoCapture(f'filesrc location={video} ! qtdemux ! queue ! h264parse ! nvv4l2decoder ! nvvidconv ! video/x-raw,format=BGRx,width=1280,height=720 ! queue ! videoconvert ! queue ! video/x-raw, format=BGR ! appsink', cv2.CAP_GSTREAMER) for video in videos]
+elif platform.machine() == "aarch64":   # For Jetson
+    decoder = "nvv4l2decoder"
+    video_converter = "nvvidconv"
     app_port = 3030
+
+# Setup the cameras
+cams = [cv2.VideoCapture(f'filesrc location={video} ! qtdemux ! queue ! h264parse ! {decoder} ! {video_converter} ! video/x-raw,format=BGRx,width=1280,height=720 ! queue ! videoconvert ! queue ! video/x-raw, format=BGR ! appsink', cv2.CAP_GSTREAMER) for video in videos]
+
     
 # colour = (B, G, R)
 colour = (0, 140, 255)
@@ -103,12 +113,17 @@ def plotdetections(detection, stream):
 def main():
     if len(cams) != batch_size:
         raise ValueError(f"Number of input streams has to be equal to the model's batch size {len(cams)} != {batch_size}")
+    
     online, frame = openstreams(cams)
     if not all(online):
         print("At least one stream can not be captured")
         return
+    
     height, width = frame[0].shape[:2]
 
+    if args.rec:
+        output_video = cv2.VideoWriter(str(video_file), fourcc, 10, (len(frame) * width, height))
+    
     fps = 0
     tau = time.time()
     smoothing = 0.9
@@ -117,11 +132,15 @@ def main():
 
     with torch.no_grad():
         while True:
-            #Capture frame-by-frame
+            # Capture frame-by-frame
             online, streams = openstreams(cams)
             streams_ok = all(online)
             if not streams_ok:
                 print("At least one stream went offline")
+                if output_video:
+                    output_video.release()
+                    [camera.release() for camera in cams]
+                    cv2.destroyAllWindows()
                 break
             # FPS calculation
             now = time.time()
@@ -129,17 +148,19 @@ def main():
                 fps = fps * smoothing + 0.1/(now - tau)
             tau = now
             
-            inputs = [cv2.resize(stream, (img_size, img_size)) for stream in streams]
-            results = model(inputs, size=img_size)
-            detections = results.pandas().xyxy
+            if args.model or args.rt_model:
+                inputs = [cv2.resize(stream, (img_size, img_size)) for stream in streams]
+                results = model(inputs, size=img_size)
+                detections = results.pandas().xyxy
 
-            streams = [plotdetections(detection, stream) for detection, stream in zip(detections, streams)]
-            # Display fps
+                streams = [plotdetections(detection, stream) for detection, stream in zip(detections, streams)]
+                # Display fps
             [cv2.putText(stream, pos, top_left, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 140, 255), 2) for pos, stream in zip(["Port", "Bow", "Starboard"], streams)]
             outs = cv2.hconcat([cv2.putText(stream, f"{int(fps)}", fps_pos, cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 2) for stream in streams])
-    
-            # if cv2.waitKey(1) & 0xFF == ord('q'): # Disabled for performance
-            #     break
+            
+            if args.rec:
+                output_video.write(outs)
+            
             if args.perf:
                 print(f"{fps:.2f}")   # Display fps  
             else:
@@ -147,7 +168,7 @@ def main():
                 outs = buffer.tobytes()
                 yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + outs + b'\r\n')
-            # print(f"{fps:.2f}")   # Display fps
+            # print(f"{fps:.2f}")   # Display fps        
             
 @app.route('/video_feed')
 def video_feed():
