fd5b68b81d147298a016f2595c214b207efb9875
Tanel Treuberg <60236942+The-Magicians-Code@users.noreply.github.com>
2022-12-30 00:03:38 +0200

    Update modelsbenchmark.py

--- dev-test/production/modelsbenchmark.py
+++ dev-test/production/modelsbenchmark.py
@@ -10,11 +10,11 @@ models_as_args = [i.split("_")[0] for i in models2]
 
 params = [i.replace("batch_", "").split("_")[1:] for i in models2]
 imsizes, batchsizes = map(list, zip(*params))
-imsizes = [i.split("x")[0] for i in imsizes]
+# imsizes = [i.split("x")[0] for i in imsizes]
 print(f"Benchmarking models: {rt_models, models}")
 
-for model in rt_models2:
-    call(f"python3 benchmark.py --rt-model {model} --precision fp16 --export".split())
+# for model in rt_models2:
+#     call(f"python3 benchmark.py --rt-model {model} --precision fp16 --export".split())
 
-for model, imsize, batchsize in zip(models_as_args, imsizes, batchsizes):
-    call(f"python3 benchmark.py --model {model} --imsize {imsize} --batch {batchsize} --export".split())
+# for model, imsize, batchsize in zip(models_as_args, imsizes, batchsizes):
+#     call(f"python3 benchmark.py --model {model} --imsize {imsize} --batch {batchsize} --export".split())
