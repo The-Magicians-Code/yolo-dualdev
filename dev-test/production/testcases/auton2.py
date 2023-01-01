efea8a53ec48a5b86fb282acf0456df667d43eb7
Tanel Treuberg <60236942+The-Magicians-Code@users.noreply.github.com>
2023-01-01 18:39:22 +0200

    Autoannotation, what can I say

--- dev-test/production/testcases/auton2.py
+++ dev-test/production/testcases/auton2.py
@@ -1,8 +1,13 @@
 import os
+import csv
 import cv2
 import torch
 from glob import glob
 from pathlib import Path
+
+model = torch.hub.load("ultralytics/yolov5", "custom", f"../models/yolov5m6_640x640_batch_1.engine") # This line is important since it contains RT execution
+input_params = model.model.bindings["images"].shape  # Retrieve input size of the model
+img_size = input_params[-1] # One dimension since n x n shape
 
 # images = glob("../datasets/*/")
 # images = images[:2]
@@ -11,10 +16,6 @@ from pathlib import Path
 # # cams = [cv2.imread(file) for file in glob(f"{images[0]}*.jpg")]
 # # print(cams[0])
 # print(Path(glob(f"{images[0]}*.jpg")[0]).with_suffix(".txt"))
-
-# model = torch.hub.load("ultralytics/yolov5", "custom", f"../models/yolov5x6_1280x1280_batch_3.engine") # This line is important since it contains RT execution
-# input_params = model.model.bindings["images"].shape  # Retrieve input size of the model
-# img_size = input_params[-1] # One dimension since n x n shape
 
 thickness = 4
 fontscale = 0.75
@@ -26,8 +27,10 @@ def make_label_folders():
         print(classes)
         [Path(f"../datasets/{folder}/labels/").mkdir(parents=True, exist_ok=True) for folder in classes]
 
-def plotdetections_x86_64(detection, stream):
-    height, width = stream.shape[:2]
+def write_detections_x86_64(detection, stream, class_id, label):
+    # height, width = stream.shape[:2]
+    dets = []
+    scores = []
     for i in range(detection.shape[0]):    
         if detection.iloc[i]["class"] != 8: # 8 - boat, ship, vessel
             continue
@@ -36,34 +39,33 @@ def plotdetections_x86_64(detection, stream):
         xmax = float(detection.iloc[i]["xmax"] / (img_size))
         ymin = float(detection.iloc[i]["ymin"] / (img_size))
         ymax = float(detection.iloc[i]["ymax"] / (img_size))
-        # print(xmin, xmax)
         confidence = detection.iloc[i]['confidence']
         score_txt = f"{(confidence * 100.0):.0f}%"
+        scores.append(score_txt)
+        dets.append([class_id, xmin, ymin, xmax, ymax])
+
+    with open(label, "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(dets)
         
-        # label = detection.iloc[i]["name"]
-
-        cv2.rectangle(stream, (int(xmin * width), int(ymin * height)), (int(xmax * width), int(ymax * height)), (0, int(confidence * 255), int(255 - confidence * 255)), thickness)
-        # cv2.rectangle(stream, (xmin, ymin), (xmax, ymax), colour, thickness)
-        # font = cv2.FONT_HERSHEY_SIMPLEX
-
-        # This block for seeing the values on detection boxes
-        # (w, h), _ = cv2.getTextSize(f"{label}: " + score_txt, font, fontscale, fontthick)
-        # cv2.rectangle(streams, (xmin, ymax - h - 10), (xmin + w, ymax), colour, -1) # -1 to fill the rectangle
-        # cv2.putText(stream, f"{label}: {score_txt}", (xmin, ymax - 5), font, fontscale, (255, 255, 255), fontthick, cv2.FILLED)
-        # cv2.putText(stream, score_txt, (xmin, ymax - 5), font, fontscale, (255, 255, 255), fontthick, cv2.FILLED)
-    
-    return stream
-
+    print(scores)
 def main():
     image_folders = glob("../datasets/*/")
     all_classes = os.listdir("../datasets")[1:]
+    # image_folders = image_folders[:1]
+    # all_classes = all_classes[0]
+    # print(image_folders, all_classes)
     for class_id, (folder, class_name) in enumerate(zip(image_folders, all_classes)):
         image_labels = [Path(image).with_suffix(".txt") for image in glob(f"{folder}*.jpg")]
-        print(class_id)
-        print(image_labels[0])
-    print(all_classes)
-        # images = [cv2.imread(file) for file in glob(f"{folder}*.jpg")]
-
-# cv2.imwrite(Path(glob(f"{images[0]}*.jpg")[0]), plotdetections_x86_64(detection, stream))
+        images = [cv2.imread(file) for file in glob(f"{folder}*.jpg")]
+        # print(class_id)
+        # print(image_labels[0])
+        inputs = [cv2.resize(image, (img_size, img_size)) for image in images]
+        for input, label in zip(inputs, image_labels):
+            results = model(input, size=img_size)
+            detections = results.pandas().xyxy
+            # print(detections[0])
+            write_detections_x86_64(detections[0], input, class_id, label)
+    # print(all_classes)
 
 main()
