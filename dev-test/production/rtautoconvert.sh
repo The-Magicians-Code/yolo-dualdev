b7d2b8fb71d7aca456f52bf4b3bcb7b46e9cbba8
Tanel Treuberg <60236942+The-Magicians-Code@users.noreply.github.com>
2023-03-22 00:41:25 +0200

    Update rtautoconvert.sh

--- dev-test/production/rtautoconvert.sh
+++ dev-test/production/rtautoconvert.sh
@@ -3,14 +3,15 @@
 # @Github: https://github.com/The-Magicians-Code
 # @Script: rtautoconvert.sh
 # @Description: Automatically converts all ONNX models in /models/ folder to TensorRT format
-# @Last modified: 2023/03/18
+# @Last modified: 2023/03/22
 
 if [ ! -f /usr/local/bin/trtexec ]; then
     echo "trtexec not set up. initialising"
     sudo cp /usr/src/tensorrt/bin/trtexec /usr/local/bin/
 fi
+
 models=$(ls models/*.onnx)
-echo $models
+not_converted=()
 
 for model in $models; do
     # Remove suffix from filename
@@ -18,6 +19,13 @@ for model in $models; do
     # Check if there exists a corresponding .engine file, if not execute trtexec
     if [ ! -f "$model".engine ]; then
         echo "$model.engine does not exist"
-        trtexec --onnx="$model".onnx --saveEngine="$model".engine --inputIOFormats=fp16:chw --outputIOFormats=fp16:chw --fp16
+        not_converted+=($model)
     fi
 done
+
+echo Number of ONNX models to convert ${#not_converted[@]}
+for model in $not_converted; do
+    trtexec --onnx="$model".onnx --saveEngine="$model".engine --inputIOFormats=fp16:chw --outputIOFormats=fp16:chw --fp16
+    echo Conversion done!
+done
+echo Done!
