22036ffb7ea2b8c76635420e4225abaf195e0ee3
Tanel Treuberg <60236942+The-Magicians-Code@users.noreply.github.com>
2023-04-03 17:24:46 +0300

    Validation set update

---
+++ dev-test/production/validate.py
@@ -0,0 +1,40 @@
+import cv2
+import os
+import torch
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder,filename))
+        if img is not None:
+            images.append(img)
+    return images
+
+imgs = load_images_from_folder("/workspace/torching/datasets/typesofships.v6i.yolov5pytorch/valid/images/")
+num_of_imgs = len(imgs)
+
+yolo = "l l6 m m6 n n6 s s6 x x6".split()
+imsize = [640, 1280]
+
+outdata = ["Model name, Input size, Accuracy"]
+
+# yolo = ["s"]
+# imsize = [640]
+for size in imsize:
+    for suffix in yolo:
+        counter = 0
+        model = torch.hub.load("ultralytics/yolov5", f"yolov5{suffix}", pretrained=True) # Load unoptimised model from Ultralytics's servers
+        for image in imgs:
+            model_input = cv2.resize(image, (size, size))
+            outs = model(model_input, size=size)
+            outs = outs.pandas().xyxy
+            if "boat" in [i for i in outs[0]["name"]]:
+                counter += 1
+        print(f"Done with yolov5{suffix}, {size}x{size}, Acc {counter / num_of_imgs}")
+        outdata.append(f"yolov5{suffix}, {size}x{size}, {counter / num_of_imgs}")
+
+print("Writing to file")
+with open(f"resultings.csv", "w") as out:
+    out.write("\n".join(outdata))
+print("Done!")
+            
