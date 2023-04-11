3bf27bfc5d3c94f49ba9810176b116643b3037b4
Tanel Treuberg <tanetre@gmail.com>
2023-04-12 02:24:37 +0300

    Opted for l4t-ml container, since it has cv2 preinstalled and compiled for CUDA, added extra pip requirements for ultralytics yolov5

--- dev-test/prod.Dockerfile
+++ dev-test/prod.Dockerfile
@@ -1,4 +1,16 @@
 ARG l4tversion
-ARG torchversion
-FROM nvcr.io/nvidia/l4t-pytorch:r$l4tversion-pth$torchversion-py3
+# ARG torchversion
+# FROM l4t-pytorch:r$l4tversion-pth$torchversion-py3
+# FROM nvcr.io/nvidia/l4t-pytorch:r$l4tversion-pth$torchversion-py3
+FROM nvcr.io/nvidia/l4t-ml:r$l4tversion-py3
+
+# ARG OPENCV_URL=https://nvidia.box.com/shared/static/5v89u6g5rb62fpz4lh0rz531ajo2t5ef.gz
+# ARG OPENCV_DEB=OpenCV-4.5.0-aarch64.tar.gz
+
+# COPY opencv_install.sh /tmp/opencv_install.sh
+# RUN cd /tmp && bash opencv_install.sh ${OPENCV_URL} ${OPENCV_DEB}
+RUN apt-get update && apt-get install python3-tk -y
+RUN pip3 install psutil tqdm
+RUN pip3 install seaborn --no-dependencies
+
 RUN ["python3"]
--- dev-test/prod_cont.sh
+++ dev-test/prod_cont.sh
@@ -2,12 +2,11 @@
 # cat /etc/nv_tegra_release
 # apt-cache show nvidia-jetpack
 
-# Set versions according to your Jetson Jetpack info
 l4tversion=32.7.1
-torchversion=1.10
+# torchversion=1.10
 
 # sudo docker pull nvcr.io/nvidia/l4t-pytorch:r${l4tversion}-pth${torchversion}-py3
 # sudo docker run -it --rm --runtime nvidia --network host nvcr.io/nvidia/l4t-pytorch:r${l4tversion}-pth${torchversion}-py3
 
-sudo docker build -t cont_test . -f prod.Dockerfile --build-arg l4tversion=$l4tversion --build-arg torchversion=$torchversion
+sudo docker build -t cont_test . -f Dockerfile --build-arg l4tversion=$l4tversion #--build-arg torchversion=$torchversion
 sudo docker run --rm -i -d --runtime nvidia --name torchcont cont_test
