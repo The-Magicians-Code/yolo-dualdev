50229dfe2bfab967d52157ca2496206d737f9afd
Tanel Treuberg <tanetre@gmail.com>
2023-04-16 01:50:10 +0300

    Automatic L4T version detection

--- dev-test/prod_cont.sh
+++ dev-test/prod_cont.sh
@@ -6,6 +6,8 @@
 # l4tversion=32.7.1
 # Jetson AGX Xavier at Jetpack 5.0.2
 l4tversion=35.1.0
+l4t=($(dpkg-query --show nvidia-l4t-core))
+l4t=${l4t[1]%-*}
 
-sudo docker build -t cont_test . -f prod.Dockerfile --build-arg l4tversion=$l4tversion
+sudo docker build -t cont_test . -f prod.Dockerfile --build-arg l4tversion=$l4t
 sudo docker run --rm -i -d --runtime nvidia -v $PWD/production:/code -p 3000:3000 --name torchcont cont_test
