1c2ef03c111cdffd9f45040d6cd95bccca8fc9b7
Tanel Treuberg <tanetre@gmail.com>
2023-04-16 02:47:27 +0300

    Automatic L4T version detection, info added

--- dev-test/prod_cont.sh
+++ dev-test/prod_cont.sh
@@ -7,10 +7,13 @@
 # and overwrite the l4t variable manually
 l4t=($(dpkg-query --show nvidia-l4t-core))
 l4t=${l4t[1]%-*}
+jetpack=($(apt-cache show nvidia-jetpack | grep "Version"))
+jetpack=${jetpack[-1]%-*}
 
+echo -e "L4T: ${l4t}\nJetpack: ${jetpack}"
 if sudo docker build -t cont_test . -f prod.Dockerfile --build-arg l4tversion=$l4t; then
-    printf "Successfully built container!\n"
+    echo Successfully built container!
     sudo docker run --rm -i -d --runtime nvidia -v $PWD/production:/code -p 3000:3000 --name torchcont cont_test
 else
-    printf "Please check the L4T and Jetpack version from https://catalog.ngc.nvidia.com/orgs/nvidia/containers/l4t-ml and overwrite the l4t variable manually\n"
+    echo Please check for the corresponding L4T version using Jetpack: ${jetpack} version from https://catalog.ngc.nvidia.com/orgs/nvidia/containers/l4t-ml and overwrite the l4t variable manually
 fi
